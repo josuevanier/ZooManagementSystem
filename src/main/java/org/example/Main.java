1d37bf6434f0d7a1870eeb4de6c76fcf9f1f07fa
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-05-21 22:14:08 -0400

    PUSHING

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,9 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+/**
+ * Not use since the zoo management system has its own void main to run
+ */
 public class Main {
-    public static void main(String[] args) {
-
-
-    }
 }
